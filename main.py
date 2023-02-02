f617c3cceafe93ddded0675c6b8e64de7d2daac6
aidiel <aidielrachman@gmail.com>
2023-02-02 10:18:23 +0700

    first commit using codebase

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Dimentional Reduction on Botnet Data"""
+"""Writen By: M. Aidiel Rachman Putra"""
+"""Organization: Net-Centic Computing Laboratory | Institut Teknologi Sepuluh Nopember"""
+
+import warnings
+warnings.simplefilter(action='ignore')
+
+import utilities.menuManagement as menu
+
+if __name__ == "__main__":
+  menu.mainMenu()
